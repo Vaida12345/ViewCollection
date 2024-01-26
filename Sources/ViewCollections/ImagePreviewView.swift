88a6552d38ed2ef71e72519fedcd7153d3d41990
Vaida <developer@vaidaf.net>
2024-01-27 02:07:28 +0800

    ImagePreviewView with scalable content

--- Sources/ViewCollections/ImagePreviewView.swift
+++ Sources/ViewCollections/ImagePreviewView.swift
@@ -16,7 +16,7 @@ import SwiftUI
 @available(iOS 17, *)
 public struct ImagePreviewView: View {
     
-    private let image: Image
+    private let image: UIImage
     
     private let onReturn: () -> Void
     
@@ -24,22 +24,47 @@ public struct ImagePreviewView: View {
     
     private let nameSpace: Namespace.ID
     
+    @State private var swapProgress: Double = 0
     
-    public var body: some View {
-        VStack {
-            image
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .matchedGeometryEffect(id: ImagePreviewView.nameSpaceID, in: nameSpace)
-                .cornerRadius(5)
-                .frame(maxHeight: 300)
-                .padding()
-            
-            HStack {
-                Button {
+    
+    @Environment(\.verticalSizeClass) private var verticalSizeClass
+    
+    
+    private var imageView: some View {
+        ScrollController(image: image)
+            .scaleEffect(.square(0.8 + 0.2 * (1 - swapProgress)))
+            .matchedGeometryEffect(id: ImagePreviewView.nameSpaceID, in: nameSpace)
+            .matchedGeometryEffect(id: "ImagePreviewView.imageView", in: nameSpace)
+    }
+    
+    private var controls: some View {
+        Group {
+            Button {
+                withAnimation(.interpolatingSpring) {
                     onReturn()
+                }
+            } label: {
+                Label("Return", systemImage: "arrow.down")
+                    .padding(.horizontal, 13)
+                    .padding(.vertical, 8)
+                    .background {
+                        Capsule()
+                            .strokeBorder(style: .init(), antialiased: true)
+                            .foregroundStyle(.ultraThickMaterial)
+                    }
+                    .background {
+                        Capsule()
+                            .fill(.ultraThinMaterial.opacity(0.5))
+                    }
+            }
+            
+            if let onDelete {
+                Button {
+                    withAnimation(.interpolatingSpring) {
+                        onDelete()
+                    }
                 } label: {
-                    Label("Return", systemImage: "arrow.down")
+                    Label("Remove", systemImage: "trash")
                         .padding(.horizontal, 13)
                         .padding(.vertical, 8)
                         .background {
@@ -51,36 +76,42 @@ public struct ImagePreviewView: View {
                             Capsule()
                                 .fill(.ultraThinMaterial.opacity(0.5))
                         }
-                }
-                
-                if let onDelete {
-                    Button {
-                        onDelete()
-                    } label: {
-                        Label("Remove", systemImage: "trash")
-                            .padding(.horizontal, 13)
-                            .padding(.vertical, 8)
-                            .background {
-                                Capsule()
-                                    .strokeBorder(style: .init(), antialiased: true)
-                                    .foregroundStyle(.ultraThickMaterial)
-                            }
-                            .background {
-                                Capsule()
-                                    .fill(.ultraThinMaterial.opacity(0.5))
-                            }
-                            .foregroundStyle(.red)
-                    }
+                        .foregroundStyle(.red)
                 }
             }
-            .transition(.move(edge: .bottom).combined(with: .opacity))
         }
-//        .onSwap(to: .bottom) {
-//            onReturn()
+        .matchedGeometryEffect(id: "ImagePreviewView.controls", in: nameSpace)
+        .animation(.interpolatingSpring, value: verticalSizeClass)
+        .transition(.offset(y: 100).combined(with: .opacity).combined(with: .scale))
+        .opacity(0.25 + (1 - swapProgress) * 0.75)
+    }
+    
+    
+    public var body: some View {
+        ZStack(alignment: verticalSizeClass == .regular ? .bottom : .trailing) {
+            imageView
+                .zIndex(-1)
+            
+            if verticalSizeClass == .regular {
+                HStack {
+                    controls
+                }
+                .padding()
+            } else {
+                VStack {
+                    controls
+                }
+                .padding()
+            }
+        }
+        .animation(.interpolatingSpring, value: verticalSizeClass)
+//        .onSwap(to: .bottom, progress: $swapProgress) {
+//            withAnimation(.interpolatingSpring) {
+//                onReturn()
+//            }
 //        }
-        .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.regularMaterial)
+        .background(.regularMaterial.opacity(0.5 + (1 - swapProgress) * 0.5))
         .ignoresSafeArea()
     }
     
@@ -92,11 +123,11 @@ public struct ImagePreviewView: View {
     ///   - onReturn: The handler when the preview should be dismissed.
     ///   - onDelete: The optional handler when the `image` should be deleted
     ///   - nameSpace: The namespace containing ``ImagePreviewView/nameSpaceID``. Used for smooth transition.
-    public init(image: Image, onReturn: @escaping () -> Void, onDelete: (() -> Void)? = nil, nameSpace: Namespace.ID) {
+    public init(image: UIImage, nameSpace: Namespace.ID, onReturn: @escaping () -> Void, onDelete: (() -> Void)? = nil) {
         self.image = image
+        self.nameSpace = nameSpace
         self.onReturn = onReturn
         self.onDelete = onDelete
-        self.nameSpace = nameSpace
     }
 
     
@@ -105,13 +136,60 @@ public struct ImagePreviewView: View {
     /// The ID can be used inside `matchedGeometryEffect` to provide a smooth transition of image.
     public static let nameSpaceID = "ImagePreviewView.image"
     
+    
+    private struct ScrollController: UIViewRepresentable {
+        
+        let image: UIImage
+        
+        func makeUIView(context: Context) -> UIScrollView {
+            let scrollView = UIScrollView()
+            scrollView.delegate = context.coordinator
+            
+            let imageView = UIImageView(image: image)
+            imageView.contentMode = .scaleAspectFit
+            imageView.frame.size = UIScreen.main.bounds.size
+            
+            scrollView.addSubview(imageView)
+            scrollView.contentSize = imageView.frame.size
+            scrollView.isScrollEnabled = true
+            scrollView.showsVerticalScrollIndicator = false
+            scrollView.showsHorizontalScrollIndicator = false
+            scrollView.maximumZoomScale = 5
+            scrollView.minimumZoomScale = 1
+            scrollView.frame = UIScreen.main.bounds
+            
+            return scrollView
+        }
+        
+        func updateUIView(_ uiView: UIScrollView, context: Context) {
+            
+        }
+        
+        
+        typealias UIViewType = UIScrollView
+        
+        final class Coordinator: NSObject, UIScrollViewDelegate {
+            
+            func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+                scrollView.subviews.first
+            }
+            
+        }
+        
+        func makeCoordinator() -> Coordinator {
+            Coordinator()
+        }
+        
+    }
+    
 }
+
 
 #Preview {
     @Namespace var nameSpace
     
     if #available(iOS 17, *) {
-        return ImagePreviewView(image: Image(systemName: "faceid"), onReturn: {}, onDelete: {}, nameSpace: nameSpace)
+        return ImagePreviewView(image: UIImage(systemName: "faceid")!, nameSpace: nameSpace, onReturn: {}, onDelete: {})
     } else {
         return EmptyView()
     }
