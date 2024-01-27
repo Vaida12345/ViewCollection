10010aaa7ccef8b49ef0eb5aee180632d34d1d62
Vaida <developer@vaidaf.net>
2024-01-27 12:44:00 +0800

    update package description

--- Package.swift
+++ Package.swift
@@ -11,19 +11,12 @@ let package = Package(
         .watchOS(.v9),
         .tvOS(.v16)
     ], products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "ViewCollection",
-            targets: ["ViewCollection"]),
+        .library(name: "ViewCollection", targets: ["ViewCollection"]),
     ], dependencies: [
         .package(name: "Nucleus", 
                  path: "~/Library/Mobile Documents/com~apple~CloudDocs/DataBase/Projects/Packages/DataBase"),
     ], targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "ViewCollection",
-                dependencies: ["Nucleus"]),
-        .testTarget(name: "ViewCollectionTests",
-                    dependencies: ["ViewCollection"]),
+        .target(name: "ViewCollection", dependencies: ["Nucleus"]),
+        .testTarget(name: "ViewCollectionTests", dependencies: ["ViewCollection"]),
     ]
 )
