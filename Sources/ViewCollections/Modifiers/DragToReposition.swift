f6079060e850aba1436b34e41dc98da7b21e66ac
Vaida <developer@vaidaf.net>
2024-10-04 01:00:25 +1000

    updates

---
+++ Sources/ViewCollections/Modifiers/DragToReposition.swift
@@ -0,0 +1,31 @@
+//
+//  DragToReposition.swift
+//  ViewCollection
+//
+//  Created by Vaida on 10/4/24.
+//
+
+import SwiftUI
+
+
+extension View {
+    
+    /// When the view is dragged, reposition the window.
+    public func dragToRepositionWindow() -> some View {
+        gesture(
+            DragGesture()
+                .onChanged { value in
+                    // Get the current window
+                    if let window = NSApplication.shared.windows.first {
+                        // Update window position
+                        let newOrigin = CGPoint(
+                            x: window.frame.origin.x + value.translation.width,
+                            y: window.frame.origin.y - value.translation.height
+                        )
+                        window.setFrameOrigin(newOrigin)
+                    }
+                }
+        )
+    }
+    
+}
