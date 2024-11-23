5b22a9484e4ab78c539bec8f21178150a1e80ec8
Vaida <developer@vaidaf.net>
2024-11-23 18:16:33 +1100

    $binding != <rhs>

--- Sources/Extensions/Binding.swift
+++ Sources/Extensions/Binding.swift
@@ -10,11 +10,33 @@ import SwiftUI
 
 extension Binding {
     
+    /// Use this method to create an binding on the equatable.
+    ///
+    /// ```swift
+    /// Toggle("Camera", systemImage: "camera.viewfinder", isOn: $popoverContent == .camera)
+    /// ```
     public static func ==<T: Sendable> (lhs: Self, rhs: Value) -> Binding<Bool> where T: Equatable, Value == Optional<T> {
         Binding<Bool> {
             lhs.wrappedValue == rhs
         } set: { newValue in
             if newValue {
+                lhs.wrappedValue = rhs
+            } else {
+                lhs.wrappedValue = nil
+            }
+        }
+    }
+    
+    /// Use this method to create an binding on the equatable.
+    ///
+    /// ```swift
+    /// Toggle("Camera", systemImage: "camera.viewfinder", isOn: $popoverContent != .camera)
+    /// ```
+    public static func !=<T: Sendable> (lhs: Self, rhs: Value) -> Binding<Bool> where T: Equatable, Value == Optional<T> {
+        Binding<Bool> {
+            lhs.wrappedValue != rhs
+        } set: { newValue in
+            if !newValue {
                 lhs.wrappedValue = rhs
             } else {
                 lhs.wrappedValue = nil
