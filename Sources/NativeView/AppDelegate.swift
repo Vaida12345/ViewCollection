f7ca5e01f0cc6b5c600daf4f6a0ab89ae196e8d6
Vaida <developer@vaidaf.net>
2024-12-13 12:26:55 +1100

    updates access control

--- Sources/NativeView/AppDelegate.swift
+++ Sources/NativeView/AppDelegate.swift
@@ -10,10 +10,10 @@ import SwiftUI
 
 #if os(macOS)
 /// A wrapper to `NSApplicationDelegateAdaptor`
-typealias ApplicationDelegateAdaptor = NSApplicationDelegateAdaptor
+public typealias ApplicationDelegateAdaptor = NSApplicationDelegateAdaptor
 #elseif os(iOS)
 /// A wrapper to `UIApplicationDelegateAdaptor`
-typealias ApplicationDelegateAdaptor = UIApplicationDelegateAdaptor
+public typealias ApplicationDelegateAdaptor = UIApplicationDelegateAdaptor
 #endif
 
 #if os(macOS)
@@ -21,10 +21,10 @@ typealias ApplicationDelegateAdaptor = UIApplicationDelegateAdaptor
 /// A wrapper to `NSApplicationDelegate`
 ///
 /// By default, the app will be closed when the last windows is closed. This could be overridden using `applicationShouldTerminateAfterLastWindowClosed`.
-public class ApplicationDelegate: NSObject, NSApplicationDelegate {
+open class ApplicationDelegate: NSObject, NSApplicationDelegate {
     
     /// Tells the delegate that the app is about to terminate.
-    func applicationWillTerminate() {
+    open func applicationWillTerminate() {
         
     }
     
@@ -32,12 +32,11 @@ public class ApplicationDelegate: NSObject, NSApplicationDelegate {
 
 extension ApplicationDelegate {
     
-    public func applicationWillTerminate(_ notification: Notification) {
+    open func applicationWillTerminate(_ notification: Notification) {
         self.applicationWillTerminate()
     }
     
-    @objc
-    public func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    open func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         true
     }
     
