0acb5593e69678dc6a5d19fc77f343b2fc063f48
Vaida <developer@vaidaf.net>
2025-06-05 00:57:19 +1000

    updates rect

--- Sources/Extensions/View + CoreGraphics.swift
+++ Sources/Extensions/View + CoreGraphics.swift
@@ -5,20 +5,57 @@
 //  Created by Vaida on 2025-05-31.
 //
 
-import SwiftUICore
+import SwiftUI
 import CoreGraphics
 
 
 extension View {
     
+    /// Positions this view within an invisible frame with the specified size.
+    ///
+    /// Use this method to specify a fixed size for a view's width and height.
+    ///
+    /// - Parameters:
+    ///   - size: A fixed width and height for the resulting view.
+    ///   - alignment: The alignment of this view inside the resulting frame.
     public func frame(_ size: CGSize, alignment: Alignment = .center) -> some View {
         self.frame(width: size.width, height: size.height, alignment: alignment)
     }
     
-    public func frame(_ frame: CGRect, alignment: Alignment = .center) -> some View {
+    /// Positions this view at the specified rect in its parent's coordinate space.
+    ///
+    /// This view uses the same coordinate plane as in `CoreGraphics`, with the origin located at the top left corner.
+    ///
+    /// ```swift
+    /// ZStack {
+    ///     DebugGridView()
+    ///     Rectangle()
+    ///         .frame(CGRect(x: 10, y: 10, width: 20, height: 20))
+    /// }
+    /// .frame(width: 100, height: 100)
+    /// ```
+    ///
+    /// ![Preview](frame)
+    ///
+    /// - Parameters:
+    ///   - rect: The rect position containing the view.
+    ///   - alignment: The alignment of this view inside `rect`.
+    public func frame(_ rect: CGRect, alignment: Alignment = .center) -> some View {
         self
-            .position(frame.origin)
-            .frame(frame.size, alignment: alignment)
+            .frame(rect.size, alignment: alignment)
+            .position(rect.center)
     }
     
+}
+
+
+#Preview {
+    ZStack {
+        DebugGridView()
+        
+        Rectangle()
+            .frame(CGRect(x: 10, y: 10, width: 20, height: 20))
+    }
+    .frame(width: 101, height: 101)
+    .padding()
 }
---
+++ Sources/Views/Standalone/DebugGridView.swift
@@ -0,0 +1,43 @@
+//
+//  DebugGridView.swift
+//  ViewCollection
+//
+//  Created by Vaida on 2025-06-05.
+//
+
+import SwiftUI
+
+
+public struct DebugGridView: View {
+    
+    private let stride: Int
+    
+    public var body: some View {
+        Canvas { context, size in
+            func width(_ i: Int) -> CGFloat {
+                if i % 100 == 0 { return 1 }
+                if i % 10 == 0 { return 0.1 }
+                if i % 1000 == 0 { return 3 }
+                return 0.01
+            }
+            
+            for i in Swift.stride(from: 0, through: size.width, by: 10) {
+                context.fill(CGRect(x: i, y: 0, width: width(Int(i)), height: size.height), with: .foreground)
+            }
+            for i in Swift.stride(from: 0, through: size.height, by: 10) {
+                context.fill(CGRect(x: 0, y: i, width: size.width, height: width(Int(i))), with: .foreground)
+            }
+        }
+    }
+    
+    public init(stride: Int = 10) {
+        self.stride = stride
+    }
+    
+}
+
+
+#Preview {
+    DebugGridView()
+}
+
