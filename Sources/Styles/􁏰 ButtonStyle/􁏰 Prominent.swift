f57aad9c17dfbfcc87f059a541696f4efedd38db
Vaida <developer@vaidaf.net>
2026-04-11 18:29:57 +1000

    Fixed ProminentButtonStyle on visionOS

--- Sources/Styles/􁏰 ButtonStyle/􁏰 Prominent.swift
+++ Sources/Styles/􁏰 ButtonStyle/􁏰 Prominent.swift
@@ -10,11 +10,15 @@ import SwiftUI
 public struct ProminentButtonStyle: PrimitiveButtonStyle {
     
     public func makeBody(configuration: Configuration) -> some View {
+        #if !os(visionOS)
         if #available(macOS 26, iOS 26, macCatalyst 26, watchOS 26, tvOS 26, *) {
             GlassProminentButtonStyle().makeBody(configuration: configuration)
         } else {
             BorderedProminentButtonStyle().makeBody(configuration: configuration)
         }
+        #else
+        BorderedProminentButtonStyle().makeBody(configuration: configuration)
+        #endif
     }
 }
 
