5b6338f0a9751afdde73173069aba40d880e3ad1
Vaida <developer@vaidaf.net>
2026-04-11 15:56:47 +1000

    Implement ProminentButtonStyle

---
+++ Sources/Styles/􁏰 ButtonStyle/􁏰 Prominent.swift
@@ -0,0 +1,39 @@
+//
+//  Prominent.swift
+//  ViewCollection
+//
+//  Created by Vaida on 2026-04-11.
+//
+
+import SwiftUI
+
+public struct ProminentButtonStyle: PrimitiveButtonStyle {
+    
+    public func makeBody(configuration: Configuration) -> some View {
+        if #available(macOS 26, iOS 26, macCatalyst 26, watchOS 26, tvOS 26, *) {
+            GlassProminentButtonStyle().makeBody(configuration: configuration)
+        } else {
+            BorderedProminentButtonStyle().makeBody(configuration: configuration)
+        }
+    }
+}
+
+
+extension PrimitiveButtonStyle where Self == ProminentButtonStyle {
+    
+    /// The most suitable prominent style.
+    ///
+    /// On pre OS 26 devices, returns `borderedProminent`, otherwise `glassProminent`.
+    public static var prominent: ProminentButtonStyle {
+        ProminentButtonStyle()
+    }
+    
+}
+
+
+#Preview {
+    Button("Hello") {
+        
+    }
+    .buttonStyle(.prominent)
+}
